5e3d28d4eaefad1916ed79f1be081d8110b2a715
Casliyan <Asliyan285@gmail.com>
2025-08-20 06:49:49 +0530

    Add files via upload

---
+++ vault.py
@@ -0,0 +1,35 @@
+# Smart Vault Unlocker
+
+def main():
+    print("┌───────────────────────────────┐")
+    print("│   Arduino-Smart Vault Login   │")
+    print("└───────────────────────────────┘")
+    
+    code = input("Enter unlock code: ").strip()
+    
+    if code == "8826":
+        print("\n--- STORED ACCOUNTS ---\n")
+        print("ACCOUNTS = [
+    {"username": "tintin567461",     "password": "tunbini4974#5^ (new)"},
+    {"username": "tungmuon1937",     "password": "khokhannao93874 (new)"},
+    {"username": "Kindlaodj712",     "password": "Kinlenoh9902"},
+    {"username": "rinbini3421",      "password": "sambini354@$%"},
+    {"username": "Ginbini5321",      "password": "sainibu24%##$"},
+    {"username": "Knoe22dlaodj712",  "password": "Db3725$^&2"},
+    {"username": "royatip284",       "password": "284Royatip"},
+    {"username": "dwqiod942",        "password": "249Dkofep"},
+    {"username": "nivexas124",       "password": "124Nivexas"},
+    {"username": "daky1233",         "password": "yoda2354"},
+    {"username": "nwoev102",         "password": "290Iejfoe"},
+    {"username": "qwfp921",          "password": "244Nvopw"},
+    {"username": "mkao182",          "password": "281Greooer"},
+]")
+        print("Epic   : epic_user / mypassword")
+        print("Gmail  : mailtest@gmail.com / g@mePass")
+        print("Ubisoft: gamer321 / rainbowSix")
+        print("\n-----------------------\n")
+    else:
+        print("❌ Invalid code. Access denied.")
+
+if __name__ == "__main__":
+    main()
